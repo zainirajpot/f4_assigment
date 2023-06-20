c44f9bb921254f2d14021cd6d8cff3c4e5512bcf
Zainab Rajpot <zainichohan712@gmail.com>
2023-06-20 12:35:48 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/f4_assigment/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.f4_assigment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required String title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Container(
+          height: 200,
+          width: 200,
+          color: Colors.amber,
+          decoration: BoxDecoration(
+            border: Border(
+              top: BorderSide(color: Colors.black, width: 5),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
